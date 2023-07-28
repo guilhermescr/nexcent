669ece737489d8f81744933c5a23eb1bd56c62a9
guilhermescr <guilhermechaves2005@hotmail.com>
2023-07-28 12:45:13 -0300

    feat: add first-section, our-clients and community sections

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from '../shared/shared.module';
+import { FirstSectionComponent } from './home/components/first-section/first-section.component';
+import { OurClientsComponent } from './home/components/our-clients/our-clients.component';
+import { CommunityComponent } from './home/components/community/community.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, FirstSectionComponent, OurClientsComponent, CommunityComponent],
   imports: [CommonModule, SharedModule],
   exports: [HomeComponent],
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { WrapperComponent } from './components/wrapper/wrapper.component';
 import { HeaderNavbarComponent } from './components/header-navbar/header-navbar.component';
+import { PrimaryButtonComponent } from './components/primary-button/primary-button.component';
 
 @NgModule({
-  declarations: [HeaderComponent, WrapperComponent, HeaderNavbarComponent],
+  declarations: [
+    HeaderComponent,
+    WrapperComponent,
+    HeaderNavbarComponent,
+    PrimaryButtonComponent,
+  ],
   imports: [CommonModule],
-  exports: [HeaderComponent, WrapperComponent],
+  exports: [HeaderComponent, WrapperComponent, PrimaryButtonComponent],
 })
 export class SharedModule {}
