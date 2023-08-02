135267344feea5dbd27a12955952c2f78b79ec88
guilhermescr <guilhermechaves2005@hotmail.com>
2023-08-02 18:09:31 -0300

    feat: add final sections

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -8,9 +8,12 @@ import { CommunityComponent } from './home/components/community/community.compon
 import { GetDemoComponent } from './home/components/get-demo/get-demo.component';
 import { NexcentBlogComponent } from './home/components/nexcent-blog/nexcent-blog.component';
 import { UnseenAtNexcentComponent } from './home/components/unseen-at-nexcent/unseen-at-nexcent.component';
+import { AchievementsComponent } from './home/components/achievements/achievements.component';
+import { CustomersComponent } from './home/components/customers/customers.component';
+import { HowToComponent } from './home/components/how-to/how-to.component';
 
 @NgModule({
-  declarations: [HomeComponent, FirstSectionComponent, OurClientsComponent, CommunityComponent, GetDemoComponent, NexcentBlogComponent, UnseenAtNexcentComponent],
+  declarations: [HomeComponent, FirstSectionComponent, OurClientsComponent, CommunityComponent, GetDemoComponent, NexcentBlogComponent, UnseenAtNexcentComponent, AchievementsComponent, CustomersComponent, HowToComponent],
   imports: [CommonModule, SharedModule],
   exports: [HomeComponent],
 })
